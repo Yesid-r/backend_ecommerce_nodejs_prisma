aa067cc68dbbf459e71f6101b781236e2061bf38
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-09-12 08:44:27 -0500

    add verify user to order

--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express();
 import {confirmation, create, findAll, findByIdUser} from './../controllers/order_controller.js'
+import { verifyUser } from '../controllers/utils/verifyToken.js';
 
 router.get('/', findAll)
 router.get('/:userId', findByIdUser)
-router.post('/create', create)
+router.post('/create',verifyUser ,create)
 router.post('/epayco/confirmation', confirmation)
 
 export default router
